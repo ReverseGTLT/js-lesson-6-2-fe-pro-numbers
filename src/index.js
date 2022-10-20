91baed57e6c7a75c43f3572401d2c14eb6b1f390
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-10-20 18:01:41 +0300

    Upd randomizer, added Math.floor

--- src/index.js
+++ src/index.js
@@ -56,5 +56,5 @@ export const getFlooroCeil = (number) => {
  * @returns {number}
  */
 export const randomizer = (from, to) => {
-  return from + Math.random() * (to - from)
+  return Math.floor(from + Math.random() * (to - from))
 };
